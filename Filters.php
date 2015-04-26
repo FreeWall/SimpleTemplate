603912649a779a6bbe64cb9d2c12b3dd929c0ede
FreeWall <info@michalvanek.net>
2015-04-26 20:25:38 +0200

    Adding filters: firstLower, toAscii, webalize

--- Filters.php
+++ Filters.php
@@ -6,10 +6,13 @@ class Filters {
 		"upper"      => "SimpleTemplate\\Filters::upper",
 		"lower"      => "SimpleTemplate\\Filters::lower",
 		"firstupper" => "SimpleTemplate\\Filters::firstUpper",
+		"firstlower" => "SimpleTemplate\\Filters::firstLower",
 		"truncate"   => "SimpleTemplate\\Filters::truncate",
 		"repeat"     => "SimpleTemplate\\Filters::repeat",
 		"date"       => "SimpleTemplate\\Filters::date",
-		"number"     => "SimpleTemplate\\Filters::number"
+		"number"     => "SimpleTemplate\\Filters::number",
+		"toascii"    => "SimpleTemplate\\Filters::toAscii",
+		"webalize"   => "SimpleTemplate\\Filters::webalize"
 	);
 
 	public static function applyFilter($s,$filter){
@@ -29,6 +32,10 @@ class Filters {
 
 	public static function firstUpper($s){
 		return self::upper(self::substring($s,0,1)).self::substring($s,1);
+	}
+
+	public static function firstLower($s){
+		return self::lower(self::substring($s,0,1)).self::substring($s,1);
 	}
 
 	public static function truncate($s,$maxLen,$append = "\xE2\x80\xA6"){
@@ -61,6 +68,44 @@ class Filters {
 
 	public static function number($number,$decimals = 0,$dec_point = '.',$thousands_sep = ' '){
 		return number_format((double)$number,(Validate::isNumber($decimals) ? $decimals : 0),$dec_point,$thousands_sep);
+	}
+
+	public static function toAscii($s){
+		$s = preg_replace('#[^\x09\x0A\x0D\x20-\x7E\xA0-\x{2FF}\x{370}-\x{10FFFF}]#u','',$s);
+		$s = strtr($s,'`\'"^~?',"\x01\x02\x03\x04\x05\x06");
+		$s = str_replace(
+			array("\xE2\x80\x9E", "\xE2\x80\x9C", "\xE2\x80\x9D", "\xE2\x80\x9A", "\xE2\x80\x98", "\xE2\x80\x99", "\xC2\xB0"),
+			array("\x03", "\x03", "\x03", "\x02", "\x02", "\x02", "\x04"), $s
+		);
+		if(class_exists('Transliterator') && $transliterator = \Transliterator::create('Any-Latin; Latin-ASCII')){
+			$s = $transliterator->transliterate($s);
+		}
+		if(ICONV_IMPL === 'glibc'){
+			$s = str_replace(
+				array("\xC2\xBB", "\xC2\xAB", "\xE2\x80\xA6", "\xE2\x84\xA2", "\xC2\xA9", "\xC2\xAE"),
+				array('>>', '<<', '...', 'TM', '(c)', '(R)'), $s
+			);
+			$s = @iconv('UTF-8','WINDOWS-1250//TRANSLIT//IGNORE',$s);
+			$s = strtr($s,"\xa5\xa3\xbc\x8c\xa7\x8a\xaa\x8d\x8f\x8e\xaf\xb9\xb3\xbe\x9c\x9a\xba\x9d\x9f\x9e"
+				. "\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3"
+				. "\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8"
+				. "\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf8\xf9\xfa\xfb\xfc\xfd\xfe"
+				. "\x96\xa0\x8b\x97\x9b\xa6\xad\xb7",
+				"ALLSSSSTZZZallssstzzzRAAAALCCCEEEEIIDDNNOOOOxRUUUUYTsraaaalccceeeeiiddnnooooruuuuyt- <->|-."
+			);
+			$s = preg_replace('#[^\x00-\x7F]++#','',$s);
+		}
+		else $s = @iconv('UTF-8','ASCII//TRANSLIT//IGNORE',$s);
+		$s = str_replace(array('`',"'",'"','^','~','?'),'',$s);
+		return strtr($s,"\x01\x02\x03\x04\x05\x06",'`\'"^~?');
+	}
+
+	public static function webalize($s,$charlist = null,$lower = true){
+		$s = self::toAscii($s);
+		if($lower) $s = strtolower($s);
+		$s = preg_replace('#[^a-z0-9'.preg_quote($charlist,'#').']+#i','-',$s);
+		$s = trim($s,'-');
+		return $s;
 	}
 
 	public static function length($s){
